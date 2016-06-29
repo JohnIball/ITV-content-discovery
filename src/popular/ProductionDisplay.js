eecaa37c3d6ae7d7c6afd6a757585432e12adec3
John Iball <johniball@yahoo.com>
2016-06-29 11:16:17 +0100

    Added some more info for a production display

--- src/popular/ProductionDisplay.js
+++ src/popular/ProductionDisplay.js
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
 
+// Display details of a single production
 class ProductionDisplay extends Component {
     constructor() {
         super();
     }
 
     render() {
+        // TODO request the correct image size
         return (
             <tr>
                 <td>
                     <img src = {this.props.productionData._links.image.href} width="256" />
                 </td>
                 <td>
-                    {this.props.productionData.programmeTitle}
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>{this.props.productionData.programmeTitle}</td>
+                            </tr>
+                            <tr>
+                                <td>{this.props.productionData.channel}</td>
+                            </tr>
+                            <tr>
+                                <td>{this.props.productionData.synopsis}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </td>
             </tr>
         );
